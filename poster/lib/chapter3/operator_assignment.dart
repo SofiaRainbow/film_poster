019215d74ca34541c1639f7835572dd0c521ea10
smiledRainbow <josetunen@origon.co.jp>
2020-11-24 17:47:20 +0900

    add chapter3

---
+++ poster/lib/chapter3/operator_assignment.dart
@@ -0,0 +1,21 @@
+void main() {
+  int a = 10;
+  int b = 5;
+  b ??= 10;
+  print(b);
+
+  a += 2;
+  print(a);
+
+  a -= b;
+  print(a);
+
+  a *= b;
+  print(a);
+  // a /= b;
+  a ~/= b;
+  print(a);
+
+  a %= b;
+  print(a);
+}
---
+++ poster/lib/chapter3/operator_conditional.dart
@@ -0,0 +1,10 @@
+void main() {
+  int gender = 0;
+  String str = gender == 0 ? "Male=$gender" : "Female=$gender";
+  print(str);
+
+  String a;
+  String b = "Java";
+  String c = a ?? b;
+  print(c);
+}
